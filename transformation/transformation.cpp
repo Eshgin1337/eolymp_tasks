d77f53fd0ff1c54d1327abb38b99fdb8f83cc678
Eshgin1337 <eshgin.hasanov.std@bhos.edu.az>
2023-09-13 21:32:39 +0400

    transformation problem solved

--- transformation/transformation.cpp
+++ transformation/transformation.cpp
@@ -2,9 +2,9 @@
 using namespace std;
 
 int main() {
-    int n;
+    long long n;
     cin >> n;
-    int changes = 0;
+    long long changes = 0;
     while (n > 1) {
         if (n % 2 == 0)
         {
