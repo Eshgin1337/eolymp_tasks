22dc3704bc43b316155ff510340e995d1581a10d
Eshgin <71690707+Eshgin1337@users.noreply.github.com>
2023-09-19 15:42:53 +0400

    Update PerimeterAndArea.cpp

--- PerimeterAndArea/PerimeterAndArea.cpp
+++ PerimeterAndArea/PerimeterAndArea.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    int width, length;
+    long long width, length;
     cin>>width>>length;
     cout << 2*(width + length) << " " << (width * length) << endl;
 }
